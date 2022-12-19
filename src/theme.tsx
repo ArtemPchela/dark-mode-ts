994000c12af1fb2408e0ab5de352f6b51f594c8a
ArtemPchela <pchelenkovartem@gmail.com>
2022-12-19 18:48:58 +0100

    react typescript dark mode

---
+++ src/theme.tsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react'
+
+interface ThemeProps {
+  darkIcon?: string
+  lightIcon?: string
+  altDark?: string
+  altLight?: string
+  imgWidth?: string
+  imgHeight?: string
+  myClass?: string
+}
+
+type Theme = 'dark' | 'light'
+
+const Theme = ({ darkIcon, lightIcon, altDark, altLight, imgWidth, imgHeight, myClass }: ThemeProps) => {
+  const themeFromLocalStorage = localStorage.getItem('theme')
+  const defaultTheme =
+    themeFromLocalStorage === 'light' || themeFromLocalStorage === 'dark' ? themeFromLocalStorage : 'light'
+  const [theme, setTheme] = useState<Theme>(defaultTheme)
+
+  const toggleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light')
+  }
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme)
+    localStorage.setItem('theme', theme)
+  }, [theme])
+
+  return (
+    <div className={myClass} onClick={toggleTheme}>
+      {theme === 'dark' ? (
+        <img src={darkIcon} alt={altDark} width={imgWidth} height={imgHeight} />
+      ) : (
+        <img src={lightIcon} alt={altLight} width={imgWidth} height={imgHeight} />
+      )}
+    </div>
+  )
+}
+
+export default Theme
